206074bd57021f81b44176d82c042b7286719b78
ashisheligocs <84785617+ashisheligocs@users.noreply.github.com>
2024-03-17 15:53:33 +0530

    POS changes

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -10,113 +10,97 @@ class ProductSeeder extends Seeder
 {
     public function run()
     {
-        if (DB::table('product_categories')->count() == 0) {
-            DB::table('product_categories')->insert([
+        if (DB::table('products')->count() == 0) {
+            DB::table('products')->insert([ 
                 [
-                    'name' => 'Food and Beverage',
-                    'slug' => 'food-and-beverage',
+                 'name' => 'BP',
+                 'code' => '0001',
+                 'model' => '',
+                 'barcode_symbology' => 'CODE128',
+                 'sub_cat_id' => 7,
+                 'brand_id' => 1,
+                 'unit_id' => 8,
+                 'tax_id' => null,
+                 'tax_type' => '',
+                 'regular_price' => 456.00 ,
+                 'discount' => 0,
+                 'note' => '',
+                 'alert_qty' => 1,
+                 'status' => 1,
+                ], 
+                [
+                 'name' => 'RS',
+                 'code' => '0001',
+                 'model' => '',
+                 'barcode_symbology' => 'CODE128',
+                 'sub_cat_id' => 7,
+                 'brand_id' => 1,
+                 'unit_id' => 8,
+                 'tax_id' => null,
+                 'tax_type' => '',
+                 'regular_price' => 550.00 ,
+                 'discount' => 0,
+                 'note' => '',
+                 'alert_qty' => 1,
+                 'status' => 1,
+                ], 
+            ]);
+        }
+        if (DB::table('product_categories')->count() == 0) {
+            DB::table('product_categories')->insert([ 
+                [
+                    'name' => 'Liquor',
+                    'slug' => 'liquor',
                     'code' => '1',
                     'note' => null,
                     'status' => 1,
-                ],
-                [
-                    'name' => 'Supplies',
-                    'slug' => 'cleaning-supplies',
-                    'code' => '2',
-                    'note' => null,
-                    'status' => 1,
-                ],
-                [
-                    'name' => 'Office Supplies',
-                    'slug' => 'office-supplies',
-                    'code' => '3',
-                    'note' => null,
-                    'status' => 1,
-                ],
-                [
-                    'name' => 'Print Materials',
-                    'slug' => 'print-materials',
-                    'code' => '4',
-                    'note' => null,
-                    'status' => 1,
-                ],
-                [
-                    'name' => 'General Maintenance',
-                    'slug' => 'general-maintenance',
-                    'code' => '5',
-                    'note' => null,
-                    'status' => 1,
-                ],
+                ], 
             ]);
         }
 
         if (DB::table('product_sub_categories')->count() == 0) {
             DB::table('product_sub_categories')->insert([
                 [
-                    'name' => 'Cleaning Supplies',
-                    'slug' => 'cleaning-supplies',
+                    'name' => 'Wine',
+                    'slug' => 'wine',
                     'code' => '1',
                     'note' => null,
                     'status' => 1,
                     'cat_id' => 2
                 ],
                 [
-                    'name' => 'Linens and Bedding',
-                    'slug' => 'linens-and-bedding',
-                    'code' => '2',
+                    'name' => 'Vodka',
+                    'slug' => 'vodka',
+                    'code' => '1',
                     'note' => null,
                     'status' => 1,
                     'cat_id' => 2
                 ],
                 [
-                    'name' => 'Toiletries',
-                    'slug' => 'toiletries',
-                    'code' => '3',
+                    'name' => 'Rum',
+                    'slug' => 'rum',
+                    'code' => '1',
                     'note' => null,
                     'status' => 1,
                     'cat_id' => 2
                 ],
                 [
-                    'name' => 'Kitchen Supplies',
-                    'slug' => 'kitchen-supplies',
-                    'code' => '4',
+                    'name' => 'Wiskey',
+                    'slug' => 'wisky',
+                    'code' => '1',
                     'note' => null,
                     'status' => 1,
                     'cat_id' => 2
                 ],
                 [
-                    'name' => 'Perishables',
-                    'slug' => 'perishables',
-                    'code' => '5',
+                    'name' => 'Beer',
+                    'slug' => 'beer',
+                    'code' => '1',
                     'note' => null,
                     'status' => 1,
-                    'cat_id' => 1
+                    'cat_id' => 2
                 ],
-
-                [
-                    'name' => 'Non-Perishables',
-                    'slug' => 'non-perishables',
-                    'code' => '6',
-                    'note' => null,
-                    'status' => 1,
-                    'cat_id' => 1
-                ],
-                [
-                    'name' => 'Beverages',
-                    'slug' => 'beverages',
-                    'code' => '7',
-                    'note' => null,
-                    'status' => 1,
-                    'cat_id' => 1
-                ],
-                [
-                    'name' => 'Operating Supplies',
-                    'slug' => 'operating-supplies',
-                    'code' => '8',
-                    'note' => null,
-                    'status' => 1,
-                    'cat_id' => 1
-                ],
+                
             ]);
         }
     }
