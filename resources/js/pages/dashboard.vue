93fe5b601db11bf2fe0d451ff119063dd7245996
abhieligocs <abhishek@eligocs.com>
2024-03-22 15:48:52 +0530

    chang in dashboard

--- resources/js/pages/dashboard.vue
+++ resources/js/pages/dashboard.vue
@@ -9,11 +9,8 @@
                 <div class="col-md-3 col-6 mb-4">
                   <router-link :to="{ name: 'pos.create' }" class="small-box-footer">
                     <div class="small-box mb-0">
-                      <div class="inner">
-                        <!-- Testing -->
-                        <!-- <div class="icon mb-2">
-                      <i class="fas fa-sign-in-alt"></i>
-                    </div> -->
+                      <div class="inner p-4 text-center">
+                        
                         <h3>
                           Sales
                         </h3>
@@ -57,41 +54,9 @@
                       </div>
                     </div>
                   </router-link>
-                </div>
-</div>
-<div class="row justify-content-center">
-                <div class="col-md-3 col-6 mb-4">
-                  <router-link :to="{ name: 'transferBalances.index' }" class="small-box-footer">
-                    <div class="small-box mb-0">
-                      <div class="inner p-4 text-center">
-                        <h3>
-                        <!-- <span>{{ inr }}</span> {{ dashboardSummery.cashbook }} -->
-                   Today Sales
-                      </h3>
-                        <!-- <p>
-                          {{ $t("dashboard.summery_items.balance_transfers") }}
-                        </p> -->
-                      </div>
-                    </div>
-                  </router-link>
-                </div>
-
-                <div class="col-xl-2 col-md-3 col-6">
-                  <router-link :to="{ name: 'transferBalances.index' }" class="small-box-footer">
-                    <div class="small-box mb-0">
-                      <div class="inner">
-                        <h3>
-                        <!-- <span>{{ inr }}</span> {{ dashboardSummery.cashbook }} -->
-                   Cash
-                      </h3>
-                        <!-- <p>
-                          {{ $t("dashboard.summery_items.balance_transfers") }}
-                        </p> -->
-                      </div>
-                    </div>
-                  </router-link>
-                </div>
+                </div> 
               </div>
+              
               <div class="row" v-if="$can('incharge-activities')">
                 <div class="col-4">
                   <div v-for="(data, i) in hotelItems" :key="i">
